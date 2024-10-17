9b3c889cffa2e64ae27abcba9eba95121d2129fb
Rudra Patel <78923892+RUDisCOOL@users.noreply.github.com>
2024-10-18 00:52:19 +0530

    Update c_runner.sh

--- c_runner.sh
+++ c_runner.sh
@@ -53,28 +53,40 @@ echo -e "${YELLOW}Output file: $output_file_name${NC}"
 # Compile based on file type
 if [ "$file_type" == "cpp" ]; then
     g++ "$file_name" -o "$output_file_name"
+    compile_status="$?"
 elif [ "$file_type" == "c" ]; then
     gcc "$file_name" -o "$output_file_name"
+    compile_status="$?"
 else
     echo -e "${RED}Error: Invalid file format\nFile is neither .cpp nor .c${NC}"
     exit 1
 fi
 
-# Check if the binary was successfully created
-if [ ! -f "$output_file_name" ]; then
-    echo -e "${RED}Error: Compilation failed. Binary not found.${NC}"
+# Compilation Error Handling
+if [ $compile_status -ne 0 ]; then
+    echo -e "${RED}Error: Compilation failed. Exiting!${NC}"
     exit 1
+else
+    echo -e "${GREEN}Compilation Successful${NC}"
 fi
 
 # Execute the binary
-echo -e "${GREEN}Executing the binary: ./${output_file_name}${BLUE}"
+echo -e "${GREEN}Executing the binary: ./${output_file_name}${NC}"
+start_time=$(date +%s%N)
 "./${output_file_name}"
-echo -e "${NC}"
+execution_status="$?"
+end_time=$(date +%s%N)
+
+# Execution Time
+elapsed_time_sec=$(echo "scale=3; ($end_time - $start_time) / 1000000000" | bc)
+echo -e "${BLUE}\nExecution Time: $elapsed_time_sec sec${NC}"
 
 # Check if the binary was executed successfully
-if [ $? -ne 0 ]; then
+if [ $execution_status -ne 0 ]; then
     echo -e "${RED}Error: Failed to execute ${output_file_name}.${NC}"
     exit 1
+else
+    echo -e "${GREEN}Execution Successful${NC}"
 fi
 
 # Remove the binary if the -r flag was provided
